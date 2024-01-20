fa2db05f05d6ad5b303b943b607912ec4dac9160
Victor <victor.miletskiy@gmail.com>
2024-01-20 17:55:29 +0200

    Fix disk name

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ echo ""
 echo "Setup portable HDD"
 echo ""
 sudo mkdir /mnt/Seagate8TB
-sudo mount /dev/sdb2 /mnt/Seagate8TB -o uid=1000,gid=1000,umask=000
+sudo mount /dev/sda2 /mnt/Seagate8TB -o uid=1000,gid=1000,umask=000
 sudo mkdir -p /mnt/Seagate8TB/.torrents/torrents-incomplete
 sudo mkdir -p /mnt/Seagate8TB/.torrents/torrents-complete
 sudo chown -R victor:victor /mnt/Seagate8TB/.torrents
@@ -38,7 +38,7 @@ echo "Setup startup script"
 echo ""
 sudo sed -i 's/exit 0/ /' /etc/rc.local
 # echo "sudo mount /dev/sda1 /mnt/Samsung_T5 -o uid=1000,gid=1000,umask=000" | sudo tee -a /etc/rc.local
-echo "sudo mount /dev/sdb2 /mnt/Seagate8TB -o uid=1000,gid=1000,umask=000" | sudo tee -a /etc/rc.local
+echo "sudo mount /dev/sda2 /mnt/Seagate8TB -o uid=1000,gid=1000,umask=000" | sudo tee -a /etc/rc.local
 echo "ls -l /mnt/Samsung_T5" | sudo tee -a /etc/rc.local
 echo "cd /home/victor/pi-media-server && sudo docker-compose up -d" | sudo tee -a /etc/rc.local
 echo "exit 0" | sudo tee -a /etc/rc.local
